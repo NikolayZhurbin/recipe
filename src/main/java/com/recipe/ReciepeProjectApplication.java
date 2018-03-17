893037769b0da8af958a6678b46884fe1f6fec13
Nikolay Zhurbin <bigboa@bk.ru>
2018-03-17 15:10:57 +0300

    Initial commit

---
+++ src/main/java/com/recipe/ReciepeProjectApplication.java
@@ -0,0 +1,12 @@
+package com.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciepeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciepeProjectApplication.class, args);
+	}
+}
